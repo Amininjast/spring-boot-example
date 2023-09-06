3e8ea6586d182992f828161996f622c8e13dd3e2
Amininjast <aminarad228@gmail.com>
2023-09-06 20:57:21 +0330

    amigos code 318 done. refactor to (API-Business Layer-DAO Layer)pattern design.

---
+++ src/main/java/com/amininjast/customer/CustomerDataAccessServise.java
@@ -0,0 +1,40 @@
+package com.amininjast.customer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerDataAccessServise implements CustomerDao {
+
+    private static List<Customer> customers;
+
+    static {
+        customers = new ArrayList<>();
+        Customer amin = new Customer(
+                1,
+                "Amin",
+                "Amin@gmail.com",
+                28
+        );
+        customers.add(amin);
+        Customer maryam = new Customer(
+                2,
+                "maryam",
+                "maryam@gmail.com",
+                22
+        );
+        customers.add(maryam);
+    }
+
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return customers.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst();
+    }
+}
