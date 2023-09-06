03469cd76f544aa50899f97104f8482014a573ff
Amininjast <aminarad228@gmail.com>
2023-09-06 19:31:16 +0330

    amigos code 316 done. using @PathVariable and stream to filter.

--- src/main/java/com/amininjast/Main.java
+++ src/main/java/com/amininjast/Main.java
@@ -3,6 +3,7 @@ package com.amininjast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -45,6 +46,17 @@ public class Main {
     @GetMapping(path = "api/v1/customers")
     public List<Customer> getCustomers() {
         return customers;
+    }
+
+    @GetMapping(path = "api/v1/customers/{customerId}")
+    public Customer getCustomer(
+            @PathVariable("customerId") Integer customerId) {
+        Customer customer = customers.stream()
+                .filter(c -> c.id.equals(customerId))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException("customer with id [%s] not found".formatted(customerId)));
+        return customer;
     }
 
     static class Customer {
