e80857242a2b4315eed24eb8da57c9040b1d0131
Amininjast <aminarad228@gmail.com>
2023-09-07 11:52:58 +0330

    amigos code 321 done. Spring Bean.

--- src/main/java/com/amininjast/Main.java
+++ src/main/java/com/amininjast/Main.java
@@ -2,13 +2,20 @@ package com.amininjast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String a : beanDefinitionNames) {
+            System.out.println(a);
+        }
+
     }
 
 }
