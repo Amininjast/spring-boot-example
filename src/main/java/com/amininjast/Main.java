7814b95cab43d079d9c77e9c6f1f38c7043e7655
Amininjast <aminarad228@gmail.com>
2023-09-07 12:22:13 +0330

    amigos code 323 done. use Spring @Bean.

--- src/main/java/com/amininjast/Main.java
+++ src/main/java/com/amininjast/Main.java
@@ -3,6 +3,7 @@ package com.amininjast;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,11 +12,23 @@ public class Main {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext applicationContext = SpringApplication.run(Main.class, args);
+        printBeanNames(applicationContext);
+
+    }
+
+    private static void printBeanNames(ConfigurableApplicationContext applicationContext) {
         String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
         for (String a : beanDefinitionNames) {
             System.out.println(a);
         }
+    }
 
+    @Bean("foo")
+    public Foo getFoo() {
+        return new Foo("this is Foo");
+    }
+
+    record Foo(String name) {
     }
 
 }
