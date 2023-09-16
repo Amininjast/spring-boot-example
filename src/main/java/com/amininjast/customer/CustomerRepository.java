ac88b0763eb9d8106598a22f2daffcf44649fffc
Amininjast <aminarad228@gmail.com>
2023-09-16 13:24:40 +0330

    amigos code 341 done. CustomerRepository Interface.

---
+++ src/main/java/com/amininjast/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.amininjast.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
+}
